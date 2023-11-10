8d23d058305caa302b41511e000efaa1f365d6d8
Joseph Mearman <joseph@mearman.co.uk>
2023-11-10 10:39:31 +0000

    Generate and write combined markdown
    This commit adds functionality to generate and write a combined markdown file that includes both the mermaid diagram and the JSON representation of the board. The generated markdown template includes a title, the mermaid diagram, and a JSON code block.

--- src/board.ts
+++ src/board.ts
@@ -52,29 +52,73 @@ percentageNode.wire("percentage", outputNode);
 export default board;
 ////////////////////////////////////////////////////////////////////////////////
 
-writeFiles(board);
+generateAndWriteCombinedMarkdown(board);
 
 function writeFiles(board: Board, name = board.title, dir = "./output") {
 	if (!name) {
 		throw new Error("Board must have a title or a name must be suplied");
 	}
 
-	writeMermaid(dir, name, board);
-	writeJson(dir, name, board);
+	generateAndWriteMarkdown(dir, name, board);
+	generateAndWriteJson(dir, name, board);
 }
 
-function writeJson(dir: string, name: string, board: Board) {
+function generateAndWriteJson(dir: string, name: string, board: Board) {
+	const jsonContent = generateJson(board);
+	writeJson(dir, name, jsonContent);
+}
+
+function writeJson(dir: string, name: string, jsonContent: string) {
 	const jsonFilePath = path.resolve(path.join(dir, "json", `${name}.json`));
 	fs.mkdirSync(path.dirname(jsonFilePath), { recursive: true });
-	const jsonContent = JSON.stringify(board, null, "\t");
 	fs.writeFileSync(jsonFilePath, jsonContent);
 	console.log("wrote", `"${jsonFilePath}"`);
 }
 
-function writeMermaid(dir: string, name: string, board: Board) {
+function generateJson(board: Board) {
+	return JSON.stringify(board, null, "\t");
+}
+
+function generateAndWriteMarkdown(dir: string, name: string, board: Board) {
+	const markdown = generateMarkdown(board);
+	writeMarkdown(dir, name, markdown);
+}
+
+function writeMarkdown(dir: string, name: string, markdown: string) {
 	const mdFilepath = path.resolve(path.join(dir, "markdown", `${name}.md`));
 	fs.mkdirSync(path.dirname(mdFilepath), { recursive: true });
-	fs.writeFileSync(mdFilepath, `\`\`\`mermaid\n${board.mermaid()}\n\`\`\``);
+	fs.writeFileSync(mdFilepath, markdown);
+	console.log("wrote", `"${mdFilepath}"`);
+}
+
+function generateMarkdown(board: Board) {
+	return `\`\`\`mermaid\n${board.mermaid()}\n\`\`\``;
+}
+
+function generateAndWriteCombinedMarkdown(board: Board, name = board.title, dir = "./output") {
+	if (!name) {
+		throw new Error("Board must have a title or a name must be suplied");
+	}
+
+	const markdownTemplate = generateCombinedMarkdown(board, name);
+	writeCombinedMarkdown(dir, name, markdownTemplate);
+}
+function generateCombinedMarkdown(board: Board, name: string = board.title) {
+	if (!name) {
+		throw new Error("Board must have a title or a name must be suplied");
+	}
+
+	const markdown = generateMarkdown(board);
+	const json = generateJson(board);
+	const jsonCodeBlock = `\`\`\`json\n${json}\n\`\`\``;
+	const markdownTemplate = `# ${name}\n\n${markdown}\n\n${jsonCodeBlock}`;
+	return markdownTemplate;
+}
+
+function writeCombinedMarkdown(dir: string, name: string, markdownTemplate: string) {
+	const mdFilepath = path.resolve(path.join(dir, "markdown", `${name}.md`));
+	fs.mkdirSync(path.dirname(mdFilepath), { recursive: true });
+	fs.writeFileSync(mdFilepath, markdownTemplate);
 	console.log("wrote", `"${mdFilepath}"`);
 }
 
